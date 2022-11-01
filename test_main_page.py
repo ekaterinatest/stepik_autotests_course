df452e675cd2cb6826c7ca6dee4cc04f75c2858d
ekaterinatest <115918944+ekaterinatest@users.noreply.github.com>
2022-11-01 22:58:38 +0400

    bug fix

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
